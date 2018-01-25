931b1a5e8637324b8f466e962bce2cb83e7d6335
anuu11 <35534904+anuu11@users.noreply.github.com>
2018-01-26 00:55:23 +0530

    Create anu34.c

---
+++ anu34.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[1000];
+int i,count 0;
+printf("enter the string\n");
+scanf("[^n]s",&a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]=='.')
+{
+count++;
+}
+printf("%d",count+1);
+}
+}
